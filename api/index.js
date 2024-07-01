1a80cc7074153618ffffb821050b7a1fd78dd4f9
adarsha <022BSCIT005@sxc.edu.np>
2024-07-01 15:11:02 +0545

    create a test api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 
 // Load environment variables from .env file
 dotenv.config();
@@ -18,3 +19,4 @@ mongoose
 app.listen(process.env.PORT, () => {
   console.log("server running on port 3002aaaaaaaa");
 });
+app.use("/api/user", userRoutes);
