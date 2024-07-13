b0f492b27450286afac5bb1de2f8b867cd7d2989
adarsha <022BSCIT005@sxc.edu.np>
2024-07-13 13:17:32 +0545

    delete user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -52,3 +52,15 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "Unauthorized"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("Deleted");
+  } catch (error) {
+    next(error);
+  }
+};
